5f78faddfea15dfbb8d7efa3e756b5f034a3afc1
x86octopus <marcia@werockitconf.com>
2020-05-30 17:21:01 -0500

    added hero banner

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="m-16">
-    <div class="border-solid border-4 border-gray-900">
-      <slot></slot>
+    <div class="container mx-auto rounded-t m-4 hero_image">      
+      <div class="text-left p-16">
+        <h3 class="font-sans text-white text-3xl mt-10">Stay connected.</h3>
+        <h1 class="font-sans tracking-wide leading-tight hm-darkblue text-6xl mt-20">Even if you're <br />a world away.</h1>
+    
+          <button class="mt-5 bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-8 border-b-4 border-teal-700 hover:border-teal-500 rounded">
+            View Kai's heartbeat, live.
+          </button>
+        
+      
+      </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
@@ -13,4 +22,12 @@ export default {
 </script>
 
 <style scoped>
+  .hero_image {
+    background-image: url('../assets/images/hero-image.jpg');
+    height: 500px;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
 </style>
