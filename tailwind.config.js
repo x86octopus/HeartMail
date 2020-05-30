4e0542e7b8ffb3f5914cc365f94038938a604fca
Raelyn Bangel <raelyn@testdouble.com>
2020-05-30 16:22:57 -0500

    add HeartMail colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
   purge: [],
   theme: {
+    extend: {
+      colors: {
+        "hm-blue": "#191716",
+        "hm-darkblue": "#191716",
+        "hm-green": "#191716",
+        "hm-lightgreen": "#191716",
+        "hm-bluegreen": "#191716",
+      },
+    },
     extend: {},
   },
   variants: {},
   plugins: [],
-}
+};
