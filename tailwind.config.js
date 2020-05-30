f478ea49f87517101a30536627a3587afd5bf36d
Raelyn Bangel <raelyn@testdouble.com>
2020-05-30 16:24:32 -0500

    correct HeartMail colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "hm-blue": "#191716",
-        "hm-darkblue": "#191716",
-        "hm-green": "#191716",
-        "hm-lightgreen": "#191716",
-        "hm-bluegreen": "#191716",
+        "hm-blue": "#55828B",
+        "hm-darkblue": "#364958",
+        "hm-green": "#87BBA2",
+        "hm-lightgreen": "#C9E4CA",
+        "hm-bluegreen": "#3B6064",
       },
     },
     extend: {},
