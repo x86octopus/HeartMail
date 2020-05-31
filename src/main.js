6d45780193f6c1e7b5c923f7c93eb3d6aade6dec
Raelyn Bangel <raelyn@testdouble.com>
2020-05-31 14:17:46 -0500

    Start live hearbeat

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./assets/styles/index.css";
-import {getHeartData} from './fitbit.js';
-import moment from 'moment'
+import { getHeartData } from "./fitbit.js";
+import moment from "moment";
+
+import { heartData } from "./heartData";
+window.heartData = heartData;
 
 Vue.config.productionTip = false;
 
