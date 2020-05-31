5a7483e0f512d712e805daabd961c728c65b0d7e
Taylor Napiontek <tn0017@uah.edu>
2020-05-30 23:13:35 -0500

    Stylized feature

---
+++ src/components/SummaryDescription.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="active">
+    <div class="font-bold text-2xl">{{title}}</div>
+    <div class="pt-6">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SummaryDescription",
+  props: {
+    active: { required: true },
+    title: { required: true }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/SummaryFeature.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="active ? 'bg-hm-darkblue text-gray-100 rounded' : ''">
+    <div class="cursor-pointer mt-2 pl-6 py-4" :id="name" @click="onClick">
+      <div class="font-bold text-lg">{{title}}</div>
+      <div>
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SummaryFeature",
+  props: {
+    active: { required: true },
+    name: { required: true },
+    title: { required: true },
+    onClick: { required: false }
+  }
+};
+</script>
+
+<style scoped>
+</style>
