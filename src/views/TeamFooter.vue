f53fd06f7ea9f896383145672ff12fb4f71a3599
x86octopus <marcia@werockitconf.com>
2020-06-01 00:06:41 -0500

    edited footer for mobile

--- src/views/TeamFooter.vue
+++ src/views/TeamFooter.vue
@@ -1,26 +1,32 @@
 <template>
 <div>
-    <div class="text-left my-10 container bg-hm-lightgreen mx-auto flex flex-col md:flex-row content-center">
-        <div class="w-1/2 mt-20 sm:mt-8 md:mt-20">
-                <div class="absolute mt-3 ml-4 sm:ml-10">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="text-hm-green fill-current w-12 h-12 md:w-16 md:h-16" viewBox="0 0 24 24"><path d="M6.5 10c-.223 0-.437.034-.65.065.069-.232.14-.468.254-.68.114-.308.292-.575.469-.844.148-.291.409-.488.601-.737.201-.242.475-.403.692-.604.213-.21.492-.315.714-.463.232-.133.434-.28.65-.35.208-.086.39-.16.539-.222.302-.125.474-.197.474-.197L9.758 4.03c0 0-.218.052-.597.144C8.97 4.222 8.737 4.278 8.472 4.345c-.271.05-.56.187-.882.312C7.272 4.799 6.904 4.895 6.562 5.123c-.344.218-.741.4-1.091.692C5.132 6.116 4.723 6.377 4.421 6.76c-.33.358-.656.734-.909 1.162C3.219 8.33 3.02 8.778 2.81 9.221c-.19.443-.343.896-.468 1.336-.237.882-.343 1.72-.384 2.437-.034.718-.014 1.315.028 1.747.015.204.043.402.063.539.017.109.025.168.025.168l.026-.006C2.535 17.474 4.338 19 6.5 19c2.485 0 4.5-2.015 4.5-4.5S8.985 10 6.5 10zM17.5 10c-.223 0-.437.034-.65.065.069-.232.14-.468.254-.68.114-.308.292-.575.469-.844.148-.291.409-.488.601-.737.201-.242.475-.403.692-.604.213-.21.492-.315.714-.463.232-.133.434-.28.65-.35.208-.086.39-.16.539-.222.302-.125.474-.197.474-.197L20.758 4.03c0 0-.218.052-.597.144-.191.048-.424.104-.689.171-.271.05-.56.187-.882.312-.317.143-.686.238-1.028.467-.344.218-.741.4-1.091.692-.339.301-.748.562-1.05.944-.33.358-.656.734-.909 1.162C14.219 8.33 14.02 8.778 13.81 9.221c-.19.443-.343.896-.468 1.336-.237.882-.343 1.72-.384 2.437-.034.718-.014 1.315.028 1.747.015.204.043.402.063.539.017.109.025.168.025.168l.026-.006C13.535 17.474 15.338 19 17.5 19c2.485 0 4.5-2.015 4.5-4.5S19.985 10 17.5 10z"/></svg>
-                </div>
-                <div class="h-full relative px-16 py-16 sm:py-8 sm:pb-16 m:py-4">          
-                    <p class="font-sans text-hm-bluegreen text-3xl mb-12">
-                        <b>We built this!</b> Center this vertically pls. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+    <div class="text-left container mx-auto flex items-center bg-hm-green">
+        <div class="w-1/2 bg-hm-lightgreen">
+                <div class="m-8">          
+                    <p class="font-sans text-hm-bluegreen mb-6 font-bold lg:text-xl">
+                        We built this! We want to help the humans who may be isolated, unsure, and stressed by providing a tangible, on-demand, and reassuring reminder that their loved ones are safe. 
                     </p>
-                <a href="https://covid19.spaceappschallenge.org/challenges/covid-challenges/world-away/teams/2fast2curious/project" class="button bg-transparent hover:bg-hm-bluegreen text-hm-bluegreen font-semibold hover:text-white py-2 px-4 border border-hm-bluegreen hover:border-transparent rounded">
-                        View our project on NASA Space Apps >
+                    <a 
+                        href="https://covid19.spaceappschallenge.org/challenges/covid-challenges/world-away/teams/2fast2curious/project" 
+                        class="text-hm-bluegreen text-sm"
+                        target="new"
+                    > View our project on NASA Space Apps >
                     </a>
                 </div>
         </div>
-        <div class="relative bg-hm-green w-full py-2 md:py-24 md:w-1/2 flex flex-col item-center justify-center">
-            <div class="relative font-sans italic text-6xl px-6 md:py-6 md:px-1 md:w-64 md:mx-auto text-hm-lightgreen font-bold leading-tight tracking-tight mb-0 sm:m-16">
-                <span class="md:block">You are not alone.</span>
+        <div class="w-1/2">
+            <div class="italic text-hm-lightgreen font-bold leading-wide tracking-wide text-center text-4xl lg:text-5xl p-10">
+                You are not alone.
             </div>
         </div>    
     </div>
-    <div class="m-10 text-center text-hm-bluegreen underline"><a href="https://suicidepreventionlifeline.org/chat/"> It’s okay to reach out if you’re feeling hopeless.</a></div>
+    <div class="m-10 mt-16 text-center text-xs text-hm-bluegreen underline">
+        <a 
+            href="https://suicidepreventionlifeline.org/chat/"
+        > 
+            It’s okay to reach out if you’re feeling hopeless.
+        </a>
+        </div>
 </div>
 </template>
 
